ec65dd1caf59c03f3ab4148661f9423787d99079
Rowan Molony <rowan.molony@codema.ie>
2021-09-08 08:21:51 +0100

    Rename extract network to extract dublin network

--- electricity-grid-capacity/tasks.py
+++ electricity-grid-capacity/tasks.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 from typing import Any
-from typing import List
 
 import geopandas as gpd
 import pandas as pd
+
 
 from ops.convert import convert_dataframe_to_geodataframe
 
@@ -25,7 +25,7 @@ def extract_dublin_substations(upstream: Any, product: Any) -> None:
     dublin_lv_substations.to_file(str(product), driver="GPKG")
 
 
-def extract_mv_and_lv_network(upstream: Any, product: Any) -> None:
+def extract_dublin_mv_and_lv_network(upstream: Any, product: Any) -> None:
     index_filepath = upstream["download_dublin_mv_index_ids"]
     dirpath = Path(upstream["check_electricity_grid_cad_data_exists"])
 
@@ -35,4 +35,4 @@ def extract_mv_and_lv_network(upstream: Any, product: Any) -> None:
         for id in mv_index_ids
     ]
     network = gpd.GeoDataFrame(pd.concat(networks), crs="EPSG:29903").to_crs(epsg=2157)
-    network.to_file(str(product["gpkg"]), driver="GPKG")
+    network.to_file(str(product), driver="GPKG")
