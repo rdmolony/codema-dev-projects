04271db97c1edf34587fd3593464630859c9b492
Rowan Molony <rowan.molony@codema.ie>
2021-09-09 11:49:54 +0100

    from networkx.algorithms.cluster import clustering
    Find nearest nodes to each station on network
    
    So can calculate network distances between nodes for use in substation clustering
    to add a network constraint!

--- electricity-grid-capacity/tasks.py
+++ electricity-grid-capacity/tasks.py
@@ -3,9 +3,12 @@ from typing import Any
 
 import geopandas as gpd
 import pandas as pd
+from shapely.geometry import Point
 
 
 from ops.convert import convert_dataframe_to_geodataframe
+from ops.io import read_networkx_gpkg
+from ops.join_nearest import join_nearest_points
 
 
 def extract_dublin_substations(upstream: Any, product: Any) -> None:
@@ -46,3 +49,25 @@ def extract_network_lines(upstream: Any, product: Any) -> None:
     # explode converts multi-part geometries to single-part which is req by networkx
     network_lines = network.query("Level in [1, 2, 10, 11]").explode()
     network_lines.to_file(str(product), driver="GPKG")
+
+
+def find_nearest_nodes_to_stations_on_network(upstream: Any, product: Any) -> None:
+    lv_substations = (
+        gpd.read_file(str(upstream["extract_dublin_substations"]))
+        .query("`Voltage Class` == 'LV'")
+        .reset_index(drop=True)
+    )
+    G = read_networkx_gpkg(str(upstream["extract_network_lines"]))
+
+    nodes_as_points = gpd.GeoSeries(
+        [Point(n) for n in G.nodes()], crs="EPSG:2157", name="geometry"
+    )
+    nearest_node_points = join_nearest_points(
+        lv_substations[["geometry"]], nodes_as_points
+    )
+    nearest_node_ids = (
+        nearest_node_points.geometry.apply(lambda x: str(x.coords[0]))
+        .rename("nearest_node_ids")
+        .to_frame()
+    )
+    nearest_node_ids.to_parquet(product)
